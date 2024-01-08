b3bb46d5737f74d1635a90b2d05c1e59179bad5e
JuanDiegoBA <jbricenoa92@gmail.com>
2024-01-08 18:52:23 -0500

    first commit

---
+++ src/main/java/com/insideback/insideback/InsidebackApplication.java
@@ -0,0 +1,13 @@
+package com.insideback.insideback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsidebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsidebackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insideback/insideback/InsidebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insideback.insideback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsidebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
